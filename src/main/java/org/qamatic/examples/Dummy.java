00d4fb612c72e9af5da8cb418c9c635fee484f0e
Senthil Maruthaiappan <senips@gmail.com>
2015-11-12 08:51:36 -0500

    context code

--- src/main/java/org/qamatic/examples/Dummy.java
+++ src/main/java/org/qamatic/examples/Dummy.java
@@ -3,5 +3,6 @@ package org.qamatic.examples;
 /**
  * Created by Senthil Maruthaiappan on 11/12/15.
  */
+
 public class Dummy {
 }
---
+++ src/test/java/org/qamatic/examples/BaseDbTest.java
@@ -0,0 +1,15 @@
+package org.qamatic.examples;
+
+/**
+ * Created by Senthil Maruthaiappan on 11/12/15.
+ */
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@ContextConfiguration(locations = {"classpath:/database-context.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BaseDbTest {
+}
