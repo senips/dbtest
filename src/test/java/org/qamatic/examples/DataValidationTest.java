882365bb456deb21bd394ff3fa6f48ee7e1d2e67
Senthil Maruthaiappan <senips@gmail.com>
2015-11-19 21:20:53 -0500

    maven fix

---
+++ src/test/java/org/qamatic/examples/DataValidationTest.java
@@ -0,0 +1,33 @@
+package org.qamatic.examples;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertNotNull;
+
+
+public class DataValidationTest extends BaseDbTest {
+
+    @Autowired
+    private Dummy injectedObject;
+
+
+    @BeforeClass
+    public static void setUp() {
+        System.out.println("-----> SETUP <-----");
+    }
+
+    @AfterClass
+    public static void afterTest() {
+        System.out.println("-----> DESTROY <-----");
+    }
+
+    @Test
+    public void testSampleServiceGetOrder() {
+
+
+        assertNotNull("Object is not null", injectedObject);
+    }
+}
